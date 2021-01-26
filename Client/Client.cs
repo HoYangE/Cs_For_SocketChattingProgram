51dd70d7e67c54a4b9047a09d9656d36de29fbea
HoYangE <syhwms@naver.com>
2021-01-26 17:03:32 +0900

    New Project

---
+++ Client/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint serverIPAddr = new IPEndPoint(IPAddress.Loopback, 12345);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            server.Connect(serverIPAddr);
+
+            string HelloTextBuf;
+            byte[] data = new byte[512];
+
+            int receiveSize = server.Receive(data);
+            Console.WriteLine(Encoding.Default.GetString(data).Substring(0, receiveSize));
+
+            while (true)
+            {
+                HelloTextBuf = Console.ReadLine();
+                if (HelloTextBuf == "end")
+                    break;
+                if (HelloTextBuf.Length > 512)
+                    continue;
+                server.Send(Encoding.Default.GetBytes(HelloTextBuf.Substring(0, HelloTextBuf.Length)));
+            }
+
+            server.Close();
+            server.Close();
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint serverIPAddr = new IPEndPoint(IPAddress.Any, 12345);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            server.Bind(serverIPAddr);
+            server.Listen(20);
+
+            Console.WriteLine("Watting Client");
+
+            Socket client = server.Accept();
+            IPEndPoint clientIPAddr = (IPEndPoint)client.RemoteEndPoint;
+            Console.WriteLine("Accept by : {0}", clientIPAddr.Address);
+
+            String HelloTextBuf = "Injoy Server";
+            byte[] data = new byte[512];
+
+            client.Send(Encoding.Default.GetBytes(HelloTextBuf.Substring(0, HelloTextBuf.Length)));
+
+            while (true)
+            {
+                int receiveSize = client.Receive(data);
+                if (receiveSize < 0)
+                    break;
+                Console.WriteLine(Encoding.Default.GetString(data).Substring(0, receiveSize));
+            }
+
+            client.Close();
+            server.Close();
+        }
+    }
+}
