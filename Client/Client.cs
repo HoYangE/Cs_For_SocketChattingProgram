60383a08b5e38e885cb8db49481fe73baa3954d4
HoYangE <syhwms@naver.com>
2021-02-02 20:29:27 +0900

    OneServerMultiClientAndSingUP/IN

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,25 +1,168 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace Client
 {
     class Client
     {
+        static bool SingUpApproved = false;
+        static object thisLock = new object();
+
+        static string SingUp(Socket server)
+        {
+            string userName, password;
+            string HelloTextBuf;
+            while (true)
+            {
+                Console.Write("Input UserName Please : ");
+                userName = Console.ReadLine();
+                Console.Write("Input Password Please : ");
+                password = Console.ReadLine();
+
+                if (CheckEnglishNumber(userName) == false || CheckEnglishNumber(password) == false)
+                    continue;
+
+                HelloTextBuf = "SingUp" + ":" + userName + ":" + password;
+                if (HelloTextBuf.Length > 512)
+                    continue;
+                server.Send(Encoding.Default.GetBytes(HelloTextBuf.Substring(0, HelloTextBuf.Length)));
+                break;
+            }
+            return userName;
+        }
+
+        static string SingIn(Socket server)
+        {
+            string userName, password;
+            string HelloTextBuf;
+            while (true)
+            {
+                Console.Write("Input UserName Please : ");
+                userName = Console.ReadLine();
+                Console.Write("Input Password Please : ");
+                password = Console.ReadLine();
+
+                if (CheckEnglishNumber(userName) == false || CheckEnglishNumber(password) == false)
+                    continue;
+
+                HelloTextBuf = "SingIn" + ":" + userName + ":" + password;
+                if (HelloTextBuf.Length > 512)
+                    continue;
+                server.Send(Encoding.Default.GetBytes(HelloTextBuf.Substring(0, HelloTextBuf.Length)));
+                break;
+            }
+            return userName;
+        }
+
+        static bool CheckEnglishNumber(string letter)
+        {
+            bool isCheck = true;
+
+            for (int i = 0; i < letter.Length; i++)
+            {
+                if (!(letter[i] >= 'A' && letter[i] <= 'Z') && !(letter[i] >= 'a' && letter[i] <= 'z') && !(letter[i] >= '0' && letter[i] <= '9'))
+                {
+                    isCheck = false;
+                }
+            }
+            return isCheck;
+        }
+
+        static void ReceiveData(Socket server)
+        {
+            SingChoice(server);
+
+            while (true)
+            {
+                byte[] data = new byte[512];
+                try
+                {
+                    int receiveSize = server.Receive(data);
+                    if (receiveSize < 0 || receiveSize > 512)
+                        break;
+                    string receiveDataString = Encoding.Default.GetString(data).Substring(0, receiveSize);
+
+                    if (receiveDataString == "Username is Already in Use")
+                    {
+                        Console.WriteLine("Username is Already in Use!");
+                        Console.WriteLine("Please SingUp Again");
+                        SingChoice(server);
+                    }
+                    else if (receiveDataString == "Username or Password Wrong Input")
+                    {
+                        Console.WriteLine("Username or Password Wrong Input!");
+                        Console.WriteLine("Please SingIn Again");
+                        SingChoice(server);
+                    }
+                    else
+                    {
+                        lock (thisLock)
+                            SingUpApproved = true;
+                        Console.WriteLine(Encoding.Default.GetString(data).Substring(0, receiveSize));
+                    }
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine("Error Message : " + err.Message);
+                    break;
+                }
+            }
+            server.Close();
+        }
+
+        static string SingChoice(Socket server) 
+        {
+            string userName = "";
+
+            Console.WriteLine("SingUp or SingIn");
+            while (true)
+            {
+                string singChoice = Console.ReadLine();
+                if (singChoice == "SingUp")
+                {
+                    userName = SingUp(server);
+                    break;
+                }
+                else if (singChoice == "SingIn")
+                {
+                    userName = SingIn(server);
+                    break;
+                }
+                else
+                    continue;
+            }
+            return userName;
+        }
+
         static void Main(string[] args)
         {
             IPEndPoint serverIPAddr = new IPEndPoint(IPAddress.Loopback, 12345);
             Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            server.Connect(serverIPAddr);
+            server.Connect(serverIPAddr); 
 
             string HelloTextBuf;
             byte[] data = new byte[512];
 
-            int receiveSize = server.Receive(data);
-            Console.WriteLine(Encoding.Default.GetString(data).Substring(0, receiveSize));
+            Thread thread = new Thread(() => ReceiveData(server));
+            thread.Start();
+
+            //int receiveSize = server.Receive(data);
+            //Console.WriteLine(Encoding.Default.GetString(data).Substring(0, receiveSize));
+
+            while (true) 
+            {
+                lock (thisLock)
+                {
+                    if (SingUpApproved == true)
+                        break;
+                }
+            }
 
             while (true)
             {
@@ -31,7 +174,6 @@ namespace Client
                 server.Send(Encoding.Default.GetBytes(HelloTextBuf.Substring(0, HelloTextBuf.Length)));
             }
 
-            server.Close();
             server.Close();
         }
     }
--- Server/Server.cs
+++ Server/Server.cs
@@ -3,40 +3,272 @@ using System.Collections.Generic;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
+using MySql.Data.MySqlClient;
 
 namespace Server
 {
+    class MySql 
+    {
+        public MySqlConnection ConnectSql()
+        {
+            var connectionString = "server=localhost;user=root;database=stydy_db;password=0407";
+            var connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("DB연결 완료");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return connection;
+        }
+
+        public void InsertData(MySqlConnection connection, string username, string password) 
+        {
+            string quary = string.Format("insert into mytable values('{0}','{1}')", username, password);
+            var commend = new MySqlCommand(quary, connection);
+            commend.ExecuteNonQuery();
+        }
+
+        public void AllDBReader(MySqlConnection connection) 
+        {
+            var commend = new MySqlCommand("select * from mytable", connection);
+            MySqlDataReader dataReader = commend.ExecuteReader();
+
+            string temp = string.Empty;
+            if (dataReader.Read() == false) temp = "No return\n";
+            else
+            {
+                do
+                {
+                    for (int i = 0; i < dataReader.FieldCount; i++)
+                    {
+                        if (i != dataReader.FieldCount - 1)
+                            temp += dataReader[i] + " ; ";    // parser 넣어주기
+                        else if (i == dataReader.FieldCount - 1)
+                            temp += dataReader[i] + "\n";
+                    }
+                } while (dataReader.Read());
+            }
+            Console.Write(temp);
+            dataReader.Close();
+        }
+
+        public bool FindUserName(MySqlConnection connection, string username) 
+        {
+            bool findUsername = false;
+
+            var commend = new MySqlCommand("select * from mytable", connection);
+            MySqlDataReader dataReader = commend.ExecuteReader();
+
+            if (dataReader.Read() == false) findUsername = false;
+            else
+            {
+                do
+                {
+                    for (int i = 0; i < dataReader.FieldCount; i+=2)
+                    {
+                        if (dataReader[i].ToString() == username)
+                            findUsername = true;
+                    }
+                } while (dataReader.Read());
+            }
+            dataReader.Close();
+
+            return findUsername;
+        }
+
+        public bool FindPassword(MySqlConnection connection, string username, string password)
+        {
+            bool findpassword = false;
+
+            var commend = new MySqlCommand("select * from mytable", connection);
+            MySqlDataReader dataReader = commend.ExecuteReader();
+
+            if (dataReader.Read() == false) findpassword = false;
+            else
+            {
+                do
+                {
+                    for (int i = 1; i < dataReader.FieldCount; i += 2)
+                    {
+                        if (dataReader[i-1].ToString() == username && dataReader[i].ToString() == password)
+                            findpassword = true;
+                    }
+                } while (dataReader.Read());
+            }
+            dataReader.Close();
+
+            return findpassword;
+        }
+
+        public void DropAllData(MySqlConnection connection) 
+        {
+            var commend = new MySqlCommand("truncate mytable", connection);
+            commend.ExecuteNonQuery();
+        }
+    }
+
     class Server
     {
         static void Main(string[] args)
         {
-            IPEndPoint serverIPAddr = new IPEndPoint(IPAddress.Any, 12345);
-            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket server, client;
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serverIPAddr, clientIPAddr;
+            serverIPAddr = new IPEndPoint(IPAddress.Any, 12345);
 
             server.Bind(serverIPAddr);
             server.Listen(20);
 
             Console.WriteLine("Watting Client");
 
-            Socket client = server.Accept();
-            IPEndPoint clientIPAddr = (IPEndPoint)client.RemoteEndPoint;
-            Console.WriteLine("Accept by : {0}", clientIPAddr.Address);
+            while (true)
+            {
+                client = server.Accept();
+                clientIPAddr = (IPEndPoint)client.RemoteEndPoint;
+                Console.WriteLine("Accept by : {0}", clientIPAddr.Address);               
 
-            String HelloTextBuf = "Injoy Server";
-            byte[] data = new byte[512];
-
-            client.Send(Encoding.Default.GetBytes(HelloTextBuf.Substring(0, HelloTextBuf.Length)));
+                Thread thread = new Thread(() => ReceiveData(client));
+                thread.Start();
+            }
+        }
+        static void ReceiveData(Socket client) 
+        {
+            string userName = "";
+            while (true)
+            {
+                userName = ReceiveUserData(client);
+                if (userName == "Used")
+                    continue;
+                if (userName == "userOut")
+                {
+                    client.Close();
+                    return;
+                }
+                break;
+            }
 
             while (true)
             {
-                int receiveSize = client.Receive(data);
-                if (receiveSize < 0)
+                byte[] data = new byte[512];
+                try
+                {
+                    int receiveSize = client.Receive(data);
+                    if (receiveSize < 0 || receiveSize > 512)
+                        break;
+                    Console.WriteLine(userName + " : " + Encoding.Default.GetString(data).Substring(0, receiveSize));
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine("Error Message : " + err.Message);
                     break;
-                Console.WriteLine(Encoding.Default.GetString(data).Substring(0, receiveSize));
+                }
+            }
+            client.Close();
+        }
+
+        static string ReceiveUserData(Socket client) 
+        {
+            string username, password, userChoice, userDataString;
+            char sp = ':';
+            byte[] userData = new byte[512];
+            string[] spstring = new string[3];
+            try
+            {
+                int receiveSize = client.Receive(userData);
+                userDataString = Encoding.Default.GetString(userData).Substring(0, receiveSize);
+                Console.WriteLine(userDataString);
+                spstring = userDataString.Split(sp);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error Message : " + err.Message);
             }
 
-            client.Close();
-            server.Close();
+            if (spstring[0] == "SingUp")
+            {
+                userChoice = spstring[0];
+                username = spstring[1];                
+                password = spstring[2];
+                if (CheckUserNameSql(username) == true) 
+                {
+                    String HelloTextBuf = "Username is Already in Use";
+                    client.Send(Encoding.Default.GetBytes(HelloTextBuf.Substring(0, HelloTextBuf.Length)));
+                    username = "Used";
+                }
+                else
+                {
+                    String HelloTextBuf = "Injoy Server";
+                    client.Send(Encoding.Default.GetBytes(HelloTextBuf.Substring(0, HelloTextBuf.Length)));
+                    Console.WriteLine("Client In");
+
+                    MySql mySql = new MySql();
+                    MySqlConnection connection = mySql.ConnectSql();
+                    mySql.InsertData(connection, username, password);
+                    connection.Close();
+                }
+            }
+            else if (spstring[0] == "SingIn")
+            {
+                userChoice = spstring[0];
+                username = spstring[1];
+                password = spstring[2];
+                if (CheckPasswordSql(username, password) == false)
+                {
+                    String HelloTextBuf = "Username or Password Wrong Input";
+                    client.Send(Encoding.Default.GetBytes(HelloTextBuf.Substring(0, HelloTextBuf.Length)));
+                    username = "Used";
+                }
+                else
+                {
+                    String HelloTextBuf = "Injoy Server";
+                    client.Send(Encoding.Default.GetBytes(HelloTextBuf.Substring(0, HelloTextBuf.Length)));
+                    Console.WriteLine("Client In");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("WhatSing? I don't know... May be UserOut");
+                username = "userOut";
+            }
+            return username;
+        }
+
+        static bool CheckUserNameSql(string username) 
+        {
+            bool haveUsername = false;
+            MySql mySql = new MySql();
+            MySqlConnection connection = mySql.ConnectSql();
+            
+            if(mySql.FindUserName(connection, username) == true) 
+            {
+                haveUsername = true;
+            }
+
+            connection.Close();
+            return haveUsername;        
+        }
+
+        static bool CheckPasswordSql(string username,string password)
+        {
+            bool havePassword = false;
+            MySql mySql = new MySql();
+            MySqlConnection connection = mySql.ConnectSql();
+
+            if (mySql.FindPassword(connection, username, password) == true)
+            {
+                havePassword = true;
+            }
+
+            connection.Close();
+            return havePassword;
         }
     }
 }
